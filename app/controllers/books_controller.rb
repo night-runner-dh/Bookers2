07a1b8b63c39242cbf95defa8b49eeda6e98d012
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-03-19 11:20:07 +0000

    4-25 destroy仮

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,16 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to top_path
+      redirect_to books_path
     else
       render :new
     end
+  end
+
+  def destroy
+    book = Book.find(params[:id]) 
+    book.destroy
+    redirect_to books_path
   end
 
   def index
@@ -18,13 +24,14 @@ class BooksController < ApplicationController
   end
 
   def show
+     @book = Book.find(params[:id])
   end
 
 
  private
 
   def book_params
-    params.require(:book).permit(:title, :body, )
+    params.require(:book).permit(:title, :body)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  
+ 
   root to: "homes#top"
   get 'homes/top' => 'homes#top', as: :top
   devise_for :users
   
-  resources :books, only: [:new, :index, :show, :create]
   
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   resources :books, only: [:new, :create, :index, :show,:destroy]
+   resources :users, only: [:show, :edit, :index]
+   
 end
